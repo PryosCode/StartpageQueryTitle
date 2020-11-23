ddfd363a77578464b74bd4f8fe98a781f2e9b3ba
PryosCode <info@pryoscode.net>
2020-11-23 21:45:02 +0100

    Create StartpageQueryTitle.user.js

---
+++ StartpageQueryTitle.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name        StartpageQueryTitle
+// @version     1.0
+// @description Startpage.com Search Engine Title Changer
+// @author      PryosCode
+// @namespace   https://github.com/PryosCode/StartpageQueryTitle
+// @website     https://github.com/PryosCode/StartpageQueryTitle
+// @supportURL  https://github.com/PryosCode/ProxerExport/issues
+// @icon        https://github.com/PryosCode/StartpageQueryTitle/raw/master/images/logo.png
+// @updateURL   https://github.com/PryosCode/StartpageQueryTitle/raw/master/StartpageQueryTitle.meta.js
+// @downloadURL https://github.com/PryosCode/StartpageQueryTitle/raw/master/StartpageQueryTitle.user.js
+// @license     MIT
+// @copyright   Copyright (C) 2020 PryosCode
+// @run-at      document-end
+// @match       https://www.startpage.com/*
+// ==/UserScript==
